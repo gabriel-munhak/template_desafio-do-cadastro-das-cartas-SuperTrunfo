e410ef76358d6f077666080ca9efa679a679255a
Gabriel Munhak <heclypso@gmail.com>
2026-05-27 20:59:42 +0000

    realizando o desafio de nível aventureiro.

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -4,6 +4,14 @@
 // Tema 1 - Cadastro das cartas
 // Objetivo: No nível novato você deve criar as cartas representando as cidades utilizando scanf para entrada de dados e printf para exibir as informações.
 
+float calculaDensidadePopulacional(float area, int populacao) {
+  return (float) populacao / area;
+}
+
+float calculaPibPerCapta(float pib, int populacao) {
+  return (pib * 1000000000.0) / (float) populacao; // converti o pib em bilhão
+}
+
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
   char estadoPrimeiraCarta[1], 
@@ -16,9 +24,13 @@ int main() {
 
   float areaEmKmQuadradoDaPrimeiraCarta, 
         pibDaPrimeiraCarta, 
+        decidadePopulacionalDaPrimeiraCarta,
+        pibPerCapitaDaPrimeiraCidade,
 
         areaEmKmQuadradoDaSegundaCarta, 
-        pibDaSegundaCarta;
+        pibDaSegundaCarta,
+        decidadePopulacionalDaSegundaCarta,
+        pibPerCapitaDaSegundaCidade;
 
   int quantidadeDePontosTuristicosPrimeiraCarta, 
       quantidadeDePontosTuristicosSegundaCarta,
@@ -62,9 +74,11 @@ int main() {
   printf("Codigo: %s%s\n", estadoPrimeiraCarta, codigoDaPrimeiraCarta);
   printf("Nome da Cidade: %s\n", nomeDaCidadeDaPrimeiraCarta);
   printf("População: %d\n", populacaoDaPrimeiraCarta);
-  printf("Área: %f\n", areaEmKmQuadradoDaPrimeiraCarta);
-  printf("PIB: %f\n", pibDaPrimeiraCarta);
+  printf("Área: %.2f km²\n", areaEmKmQuadradoDaPrimeiraCarta);
+  printf("PIB: %.2f bilhões de reais\n", pibDaPrimeiraCarta);
   printf("Quantidade de Pontos Turisticos: %d\n", quantidadeDePontosTuristicosPrimeiraCarta);
+  printf("Densidade Populacional: %.2f hab/km²\n", calculaDensidadePopulacional(areaEmKmQuadradoDaPrimeiraCarta, populacaoDaPrimeiraCarta));
+  printf("PIB per Capita: %.2f reais\n", calculaPibPerCapta(pibDaPrimeiraCarta, populacaoDaPrimeiraCarta));
 
   // Área para entrada de dados
 
@@ -102,9 +116,11 @@ int main() {
   printf("Codigo: %s%s\n", estadoSegundaCarta, codigoDaSegundaCarta);
   printf("Nome da Cidade: %s\n", nomeDaCidadeDaSegundaCarta);
   printf("População: %d\n", populacaoDaSegundaCarta);
-  printf("Área: %f\n", areaEmKmQuadradoDaSegundaCarta);
-  printf("PIB: %f\n", pibDaSegundaCarta);
+  printf("Área: %.2f km²\n", areaEmKmQuadradoDaSegundaCarta);
+  printf("PIB: %.2f bilhões de reais\n", pibDaSegundaCarta);
   printf("Quantidade de Pontos Turisticos: %d\n", quantidadeDePontosTuristicosSegundaCarta);
+  printf("Densidade Populacional: %.2f hab/km²\n", calculaDensidadePopulacional(areaEmKmQuadradoDaSegundaCarta, populacaoDaSegundaCarta));
+  printf("PIB per Capita: %.2f reais\n", calculaPibPerCapta(pibDaSegundaCarta, populacaoDaSegundaCarta));
 
   printf("\n-- Obrigado por criar cartas conosco --\n\n");
 
